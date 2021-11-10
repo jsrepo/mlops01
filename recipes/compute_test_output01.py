7713a2f6a10e0e79466951219d7d5747f27070dc
admin <jiseong.kim@dataiku.com>
2021-11-10 05:44:38 +0000

    Recipe OCBETAMODELDOCUMENTATION.compute_test_output01 saved through notebook notebook editor for compute_test_output01

--- recipes/compute_test_output01.py
+++ recipes/compute_test_output01.py
@@ -1,17 +1,20 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 westnile_joined_by_Trap_prepared = dataiku.Dataset("westnile_joined_by_Trap_prepared")
 westnile_joined_by_Trap_prepared_df = westnile_joined_by_Trap_prepared.get_dataframe()
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 test_output01_df = westnile_joined_by_Trap_prepared_df # For this sample code, simply copy input to output
 
 
